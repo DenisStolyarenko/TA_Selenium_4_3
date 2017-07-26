fb6233d8de032921d67aab739c98884d8724e35e
Denis Stolyarenko <denis_stolyarenko@epam.com>
2017-07-26 09:43:11 +0600

    Update project structure

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,15 +1,10 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import services.Driver;
-import services.Screenshoter;
 
-import javax.swing.*;
 import java.util.List;
 
 public class MainPage extends AbstractPage{
